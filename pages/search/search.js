456c7dc2f506bb411f172a2d693f370312890a0f
rosen <rosen101721dz@126.com>
2019-12-27 23:17:46 +1100

    Fuzzy search
    fuzzy search ___ completed

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,17 +1,69 @@
 // pages/search/search.js
+
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    input:'',
+    searchList: [],
   },
-  searchSubmit: function (e) {
-    var idx = parseInt(e.currentTarget.id);
-    wx.navigateTo({
-      url: 'searchResults/searchResults?str=' + idx,
+  searchInput:function(e){
+    this.setData({
+      input: e.detail.value
     })
+  },
+  searchSubmit: function () {
+    console.log(this.data.input)
+    //Search in the database
+    let that = this
+    if (that.data.input!='')
+    {
+      //Loading
+      this.showLoading()
+      db.collection('schools').where({
+        //使用正则查询，实现对搜索的模糊查询
+        //name: that.data.input
+        //"The University of Melbourne"
+        name: db.RegExp({
+          regexp: that.data.input,
+          //从搜索栏中获取的value作为规则进行匹配。
+          options: 'i',
+          //大小写不区分
+        })
+      }).get({
+        success: res => {
+          console.log(res)
+          that.setData({
+            searchList: res.data
+          })
+        },
+        fail: err => {
+          console.log("************fail*************")
+        }
+      })
+      //this.cancelLoading()
+    }
+    //hide the loading
+
+    // var idx = parseInt(e.currentTarget.id);
+    // wx.navigateTo({
+    //   url: 'searchResults/searchResults?str=' + idx,
+    // })
+  },
+  //Loading function
+  showLoading: function () {
+    wx.showToast({
+      title: 'Loading',
+      mask: true,
+      icon: 'loading'
+    });
+  },
+  //cancel loading 
+  cancelLoading: function () {
+    wx.hideToast();
   },
   /**
    * 生命周期函数--监听页面加载
