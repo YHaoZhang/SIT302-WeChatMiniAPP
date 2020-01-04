e663c2485f46d3077a0be4554e94abd0b67e4cac
hantong <hxing@deakin.edu.au>
2020-01-04 13:28:07 +1100

    恢复登录

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,16 +24,16 @@ Page({
     this.onGetOpenid();
 
     // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.switchTab({
-    //         url: '../home/home',
-    //       })
-    //     }
-    //   }
-    // })
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.switchTab({
+            url: '../home/home',
+          })
+        }
+      }
+    })
     db.collection('students').doc("c086ceb4-c6f7-41f5-bbf0-0ef4cb9284b7").get({
       success: res => {
         app.globalData.student=res.data;
@@ -58,9 +58,6 @@ Page({
         app.globalData.applications=res.data.application;
       }
     })
-    wx.switchTab({
-      url: '../home/home',
-    })
   },
 
   bindGetUserInfo (e) {
@@ -82,15 +79,9 @@ Page({
       success: res => {
         console.log('[云函数] [login] user info: ', res)
         app.globalData.openid = res.result.openid
-        // wx.switchTab({
-        //   url: '../home/home',
-        // })
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-        // wx.navigateTo({
-        //   url: '../deployFunctions/deployFunctions',
-        // })
       }
     })
   },
